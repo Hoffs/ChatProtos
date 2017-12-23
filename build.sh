9db5db5c2d0460ca696e2c67483d03db64c1911f
hoffs <ignasmaslinskas@gmail.com>
2017-12-23 13:36:22 +0200

    added bash script

---
+++ build.sh
@@ -0,0 +1,5 @@
+mkdir "Compiled\Networking\Messages"
+mkdir "Compiled\Data"
+./protoc -I="." --csharp_out="Compiled/Networking/" Networking/*.proto --csharp_opt=file_extension=.g.cs
+./protoc -I="." --csharp_out="Compiled/Networking/Messages/" Networking/Messages/*.proto --csharp_opt=file_extension=.g.cs
+./protoc -I="." --csharp_out="Compiled/Data/" Data/*.proto --csharp_opt=file_extension=.g.cs
