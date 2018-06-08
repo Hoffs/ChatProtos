ed28780a1a6755d5c1cdd4495a47f041c123ae4c
Ignas Maslinskas <ignasmaslinskas@gmail.com>
2018-06-08 15:02:42 +0300

    Update build script to create parents

--- build_linux.sh
+++ build_linux.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-mkdir "Compiled\Networking\Messages"
-mkdir "Compiled\Data"
+mkdir -p Compiled/Networking/Messages
+mkdir -p Compiled/Data
 protoc -I="." --csharp_out="Compiled/Networking/" Networking/*.proto --csharp_opt=file_extension=.g.cs
 protoc -I="." --csharp_out="Compiled/Networking/Messages/" Networking/Messages/*.proto --csharp_opt=file_extension=.g.cs
 protoc -I="." --csharp_out="Compiled/Data/" Data/*.proto --csharp_opt=file_extension=.g.cs
